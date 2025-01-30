28a6ea62ab1a0b358ed78ee33fe17ee77c1d7a60
Lucas Von Ryn <lvonryn@gmail.com>
2025-01-30 19:52:30 -0300

    feat: Adicionei as funções de requisição do TodoController

--- laravel/app/Http/Controllers/TodoController.php
+++ laravel/app/Http/Controllers/TodoController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+    public function index()
+    {
+        return Todo::all();
+    }
+
+    public function store(Request $request)
+    {
+        $todo = Todo::create($request->all());
+        return response()->json($todo, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $todo = Todo::findOrFail($id);
+        $todo->update($request->all());
+        return response()->json($todo);
+    }
+
+    public function destroy($id)
+    {
+        Todo::destroy($id);
+        return response()->json(null, 204);
+    }
 }
