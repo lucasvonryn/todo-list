7b97b7e6b85ca054e31d84f442da5ee1d11ed99b
Lucas Von Ryn <lvonryn@gmail.com>
2025-01-30 20:46:09 -0300

    feat: Iniciei a tela de login.

--- ionic/src/app/pages/dashboard/dashboard.page.ts
+++ ionic/src/app/pages/dashboard/dashboard.page.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
+  imports: [IonicModule, CommonModule, RouterModule],
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
 })
-export class DashboardPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class DashboardPage {}
--- ionic/src/app/pages/signup/signup.page.ts
+++ ionic/src/app/pages/signup/signup.page.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
+  standalone: true,
+  imports: [IonicModule, CommonModule, RouterModule],
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
 })
-export class SignupPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class SignupPage {}
