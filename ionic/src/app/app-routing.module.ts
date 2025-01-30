a86ffd6ff405a2307975786574343d34fd274e6a
Lucas Von Ryn <lvonryn@gmail.com>
2025-01-30 20:29:57 -0300

    feat: Gerei as pages de login, signup e dashboard.

--- ionic/src/app/app-routing.module.ts
+++ ionic/src/app/app-routing.module.ts
@@ -11,6 +11,18 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
 ];
 
 @NgModule({
